95f4ee5206b713c237a8e560b4abafbaf1edda69
千橘 雫霞 <i@qianjunakasumi.ren>
2021-07-01 22:59:18 +0800

    feat: log system

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/lyexamination/api
 go 1.16
 
 require (
+	github.com/rs/zerolog v1.23.0
 	google.golang.org/grpc v1.38.0
 	google.golang.org/protobuf v1.26.0
 )
--- main.go
+++ main.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"net"
+	"os"
+	"time"
 
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
 )
 
 func main() {
 
+	log.Logger = log.
+		Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}).
+		With().Caller().
+		Logger()
+
+	log.Info().Msg("build a friendly Longyan examination App for students | welcome to join the QQ group: 422954727")
+
 	lis, err := net.Listen("tcp", ":443")
 	if err != nil {
-		panic(err)
+		log.Panic().Err(err)
 	}
+
+	log.Info().Msg("监听 :443 正常")
 
 	server := grpc.NewServer()
 	RegisterLYExaminationServer(server, LYExamination{})
 	if err := server.Serve(lis); err != nil {
-		panic(err)
+		log.Panic().Err(err)
 	}
 }
